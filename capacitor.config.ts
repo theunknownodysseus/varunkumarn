e1919e26ca31eb7507f8b2f47d51de170ec4be09
theunknownodysseus <varunify.777@gmail.com>
2026-01-30 09:29:25 +0530

    added capacitor.config.ts

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.varun777',
+  appName: 'varun',
+  webDir: 'dist'
+};
+
+export default config;
